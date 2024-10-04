9584d59d5611a6aa0ce1824ea97faf045918d0a5
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-04 15:57:09 +0300

    navigation bar completed and styled and also successfully mounted

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Classes from './NavigationBar.module.css'; 
+import MenuIcon from '../../Assets/hamburger.jpg';
+
+const NavigationBar = () => {
+  return (
+    <>
+       <nav className={Classes.lower__container}>
+        <button className={Classes.menu__button}>
+          <img src={MenuIcon} alt="Menu Icon" className={Classes.menu__icon} />
+          <span>All</span>
+        </button>
+        <ul className={Classes.nav__list}>
+          <li>Today's Deals</li>
+          <li>Customer Service</li>
+          <li>Registry</li>
+          <li>Gift Cards</li>
+          <li>Sell</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavigationBar;
