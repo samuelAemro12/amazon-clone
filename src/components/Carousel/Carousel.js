bc60938e88036cc4a20014f041521d0e65747826
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-05 20:34:04 +0300

    carousel effect and it's styling done for the homepage

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Classes from './Carousel.module.css'; 
+import { Carousel } from 'react-responsive-carousel';
+import { ImageArray } from '../Carousel/ImagesForCarousel/ImageDatamapperArray';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // the css libarary from npmjs.com by installing react-responsive-carousel 
+
+const CarouselEffect = () => {
+  return (
+    <div>
+      {/* Got react-responsive-carousel dependency */}
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {
+          ImageArray.map((ImageItemLink)=>{
+            return <img src={ImageItemLink}/>
+          })
+        }
+      </Carousel>
+      <div className={Classes.image__style}></div>
+    </div>
+  )
+}
+
+export default CarouselEffect;
