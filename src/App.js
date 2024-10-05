3f0813845e3fba9e5f3fcc5f55d86de1541a8f58
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-05 20:35:26 +0300

    CarouselEffect succesfully mounted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from '../src/components/Header/Header.js'
-
+import Header from '../src/components/Header/Header.js';
+import CarouselEffect from './components/Carousel/Carousel.js';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CarouselEffect/>
+      
     </div>
   );
 }
