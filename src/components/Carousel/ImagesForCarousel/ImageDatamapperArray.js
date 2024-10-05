37690b4ac7ce8dd1be3964020d96b1b5291b659d
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-05 20:24:02 +0300

    added the images for the carousel effect and stored them in an array in order to export them in ImageDatamapperArray.js

---
+++ src/components/Carousel/ImagesForCarousel/ImageDatamapperArray.js
@@ -0,0 +1,8 @@
+import Image1 from './ImageForCarousel-1.jpg';
+import Image2 from './ImageForCarousel-2.jpg';
+import Image3 from './ImageForCarousel-3.jpg';
+import Image4 from './ImageForCarousel-4.jpg';
+import Image5 from './ImageForCarousel-5.jpg';
+// image fetched from the amazon home page by using ImageAssistant Batch Imagwe Downloader (a google extension)
+
+export const ImageArray = [Image1, Image2, Image3, Image4, Image5 ];
