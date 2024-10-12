5c7c8d1adc60136ea0a0be8030b179f57c554a75
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-12 13:07:04 +0300

    mounted the Category component which is a card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from '../src/components/Header/Header.js';
 import CarouselEffect from './components/Carousel/Carousel.js';
+import Category from '../src/components/Category/Category.js'
 function App() {
   return (
     <div className="App">
       <Header/>
-      <CarouselEffect/>
-      
+      <CarouselEffect/> 
+      <Category/>
     </div>
   );
 }
