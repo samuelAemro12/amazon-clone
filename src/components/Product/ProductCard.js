6c5d4313c936427bb3d59b08dd3c5d2993a3d48d
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-14 01:08:05 +0300

    set the basic jsx to render cards to product component

---
+++ src/components/Product/ProductCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <a href=''>
+        <img src='' alt=''/>
+      </a>
+      <div>
+        <h3>title</h3>
+        <div>
+            {/* rating */}
+            {/* price */}
+        </div>        
+        <div>
+            {/* price */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard;
