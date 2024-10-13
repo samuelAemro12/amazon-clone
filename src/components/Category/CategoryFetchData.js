f0c8c6464cebd2084a727bd313470d88bff75eb2
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-13 22:45:59 +0300

    imported images from the assset folder and linked the images inside the CategoryFetchData object inorder to export it inside the card component

--- src/components/Category/CategoryFetchData.js
+++ src/components/Category/CategoryFetchData.js
@@ -1,22 +1,27 @@
+import Electronics from '../../Assets/Elctronics.jpg';
+import FemaleFashion from '../../Assets/female-fashion.jpg';
+import Jewelery from '../../Assets/jewelery.jpg';
+import MaleFashion from '../../Assets/male-fashion.jpg';
+
 export const CategoryFetchData =[
         {
             title:"Electronics",
             name:"electronics",
-            imageLink:""
+            image: Electronics
         },
         {
             title:"Discover Fashion Trends",
             name:"women's clothing",
-            imageLink:""
+            image: FemaleFashion
         },
         {
             title:"Men's Clothing",
             name:"men's clothing",
-            imageLink:""
+            image: MaleFashion
         },
         {
             title:"Jewelery",
             name:"jewelery",
-            imageLink:""
+            image: Jewelery
         }
 ]
