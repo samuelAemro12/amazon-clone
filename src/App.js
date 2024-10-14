3bfa7107597d57ff915ba129dd904b00ab2fcdff
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-14 20:13:07 +0300

    mounted the product component although it has some problems

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from '../src/components/Header/Header.js';
 import CarouselEffect from './components/Carousel/Carousel.js';
 import Category from '../src/components/Category/Category.js'
+import Product from './components/Product/Product.js';
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <CarouselEffect/> 
       <Category/>
+      <Product/>
     </div>
   );
 }
