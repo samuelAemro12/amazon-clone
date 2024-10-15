e2f58eb1ef31d1f6d2c5e75792404a96e0202f5a
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-16 01:17:23 +0300

    set up the page correctly after data fetching and ProductCard component incorporation and fixed the issue with the useState hook

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
-const Product = () => {
-    const [product, setproduct] = useState();
-    useEffect(()=>{
-        axios.get('https://fakestoreapi.com/product')
-        .then((res) => {
-            
-        })
-    },[]);
+import ProductCard from './ProductCard';
 
+
+const Product = () => {
+    const [products, setproducts] = useState([]);
+    useEffect(()=>{
+        axios.get('https://fakestoreapi.com/products')
+        .then((res) => {
+            setproducts(res.data);
+        }).catch((error)=>{
+            console.log(error);
+        });
+    },[]);
 return (
         <div>
-      
+            {products.map((singleProduct) => {
+                return <ProductCard key={singleProduct.id} product={singleProduct} />
+            })
+            }
         </div>
   )
 }
