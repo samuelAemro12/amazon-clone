5b77fa8dd066d39d78c185b5096ed2d0defda0ef
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-16 02:19:04 +0300

    done the formating and passed it to the ProductCard component for pricing

--- src/components/CurrencyFormat/CurrencyFormat.js
+++ src/components/CurrencyFormat/CurrencyFormat.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import numeral from 'numeral';
 
-const CurrencyFormat = () => {
-  return (
+const CurrencyFormat = ({amount}) => {
+    const FormatedAmount = numeral(amount).format("$0,0.00"); 
+    return
     <div>
-      
+        {FormatedAmount}
     </div>
-  )
 }
 
 export default CurrencyFormat;
