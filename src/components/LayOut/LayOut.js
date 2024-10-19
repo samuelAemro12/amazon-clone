86ec213bbba736ff9f3ef2b9f37b34b8c595f199
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-19 23:47:54 +0300

    fixed the issue with rendering the other component on the landing page via sending props

--- src/components/LayOut/LayOut.js
+++ src/components/LayOut/LayOut.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '../Header/Header';
 
-const LayOut = ({props}) => {
+const LayOut = ({children}) => {
   return (
     <div>
       <Header/>
-      {props}
+      {children}
     </div>
   );
 }
