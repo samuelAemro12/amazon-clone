edfd3535309124041d71b0a5d7d52696c5e4823c
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-20 01:13:41 +0300

    results page fetch and render done also styling is done

--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Classes from './Results.module.css'
 import LayOut from '../../components/LayOut/LayOut';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import {productUrl} from '../../API/endPoint'
+import ProductCard from '../../components/Product/ProductCard';
 
 const Results = () => {
+  const {catergoryName} = useParams();
+  const [result, setResult] = useState([]);
+  useEffect(()=>{
+    axios.get(`${productUrl}/products/category/${catergoryName}`)
+    .then((res)=>{
+      setResult(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  },[])
+
   return (
-    <LayOut>
-      
+    <LayOut >
+      <section>
+        <h1>Results</h1>
+        <p>Category / {catergoryName}</p>
+        <hr/>
+        <div className={Classes.product__container}>
+          {
+            result?.map((product)=>{
+              return(
+              <ProductCard key={product.id} product={product}/>
+            )})
+          }
+        </div>
+      </section>
+            
     </LayOut>
   )
 }
