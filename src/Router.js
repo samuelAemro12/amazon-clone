555b32c513bb66b8c73311c360d90f74b37ae159
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-19 23:06:57 +0300

    added router to navigate between pages and components

---
+++ src/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing/Landing';
+import SignUp from './Pages/Auth/SignUp';
+import Payment from './Pages/Payment/Payment';
+import Orders from './Pages/Orders/Orders';
+import Cart from './Pages/Cart/Cart';
+
+const Routers = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path= "/auth" element={<SignUp/>}/>
+        <Route path= "/payment" element={<Payment/>}/>
+        <Route path= "/orders" element={<Orders/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default Routers;
+;
