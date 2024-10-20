0dc44cb943a90f0209f5f3a2ae5fd5f618856011
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-20 19:58:20 +0300

    data fetching and rendering to the productDetail page is done, ProductCard rendered made possible by getting the isolated peoduct props

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Classes from './ProductDetail.module.css';
 import LayOut from '../../components/LayOut/LayOut';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { productUrl } from '../../API/endPoint';
+import ProductCard from '../../components/Product/ProductCard';
 
 const ProductDetail = () => {
+  const {productId} = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(()=>{
+    axios.get(`${productUrl}/${productId}`)
+    .then((res)=>{
+      setProduct(res.data);
+    }).catch((err)=>{
+        console.log(err);
+    })
+  },[]);
+  
+
   return (
     <LayOut>
-      
+      <ProductCard product={product}/>
     </LayOut>
   )
 }
