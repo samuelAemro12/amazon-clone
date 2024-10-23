bf8efa01ff7e764f9ee8f1f265d0886842867a56
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-24 02:56:19 +0300

    loader imported and set

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { RiseLoader } from 'react-spinners';
 const Loader = () => {
   return (
-    <div>
-      
+    <div style={{
+      display:"flex",
+      alignItems: "center",
+      height: "50vh",
+    }}>
+      <RiseLoader color="336d7b7" />
     </div>
   )
 }
