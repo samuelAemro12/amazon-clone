770a11ef64ee05ffd563122c180541d8278195a5
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-10-26 14:54:35 +0300

    made the cart page to recieve product data by effectively mapping through the basket array in the initial state, when people click add to cart button

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Classes from './Cart.module.css';
 import LayOut from '../../components/LayOut/LayOut';
+import { DataContext } from '../../components/DataProvider/DataProvider';
+import ProductCard from '../../components/Product/ProductCard';
 
 const Cart = () => {
+  const [{basket, user}, dispatch] =useContext(DataContext);
   return (
     <LayOut>
-      
+      <section>
+        <div>
+          <h2>Hello</h2>
+          <h3>Your shopping basket</h3>
+          <hr/>
+          {
+            basket?.length == 0 ?(<p>Oops ! No item in your Cart</p>)
+            :(basket?.map((item, i)=>{
+              return(<ProductCard
+                product={item}
+                key={i}
+                renderDescription={true}
+                flex={true}
+              />)
+            }))
+          }
+        </div>
+      </section>
     </LayOut>
   )
 }
