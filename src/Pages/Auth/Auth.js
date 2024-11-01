7d133d7d2e2cbc8fd56522e3ad33faeae1997f3d
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-11-02 01:29:29 +0300

    set up the firebase authenticaticn dynamic sign up

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import Classes from './Auth.module.css'
 import { Link } from 'react-router-dom';
 import Amazonimg from '../../Assets/AmazonLogoForSignUp.jpg';
+import {auth} from '../../Utility/Firebase';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {DataContext} from '../../components/DataProvider/DataProvider';
+import { Type } from '../../Utility/action.type';
+
+
 const Auth = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [{user}, dispatch] = useContext(DataContext)
+
+  const authHandler = async (e)=>{
+      e.preventDefault();
+
+      // console.log(e.target.value);
+
+    if (e.target.name == "signin") {
+      // firbase authentication
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userInformation)=>{
+          dispatch({
+            type:Type.SET_USER,
+            user:userInformation.user
+       })
+      }).catch((error)=>{
+        console.log(error);
+        
+      })
+    } else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userInformation)=>{
+          dispatch({
+            type:Type.SET_USER,
+            user:userInformation.user
+       });
+      }).carch((error)=>{
+        console.log(error);
+        
+      })
+    }
+  };
+  // console.log(password, email);
+
   return (
     <section className={Classes.signin}>
       <Link>
@@ -14,13 +59,16 @@ const Auth = () => {
         <form action=''>
           <div>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email'></input>
+            <input value={email} onChange={(e)=>setEmail(e.target.value)} type='email' id='email'></input>
           </div>
           <div>
             <label htmlFor='password'>password</label>
-            <input type='password' id='password'></input>
+            <input  value={password} onChange={(e)=>setPassword(e.target.value)} type='password' id='password'></input>
           </div>
-          <button className={Classes.signin__button}>Sign in</button>
+          <button className={Classes.signin__button}
+                  type="submit" onClick={authHandler}
+                  name='signin'
+          >Sign in</button>
         </form>
         <p>
           By Signing-in you agree to the AMAZON CLONE BY SAMUEL AEMRO conditions of use &
@@ -28,7 +76,9 @@ const Auth = () => {
           Intrest-Based Ads Notice.
         </p>
 
-        <button className={Classes.account__create__button}>Create your Amazon account</button>
+        <button className={Classes.account__create__button}
+                type="submit" onClick={authHandler} name='signun'
+        >Create your Amazon account</button>
       </div>
     </section>
   )
