3e0ca16da873047d0443d12b80868c01a1de4021
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-11-05 21:21:59 +0300

    sign in state preservance added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import './App.css';
 import Routers from '../src/Router';
+import { DataContext } from './components/DataProvider/DataProvider';
+import { Type } from './Utility/action.type';
+import { auth } from './Utility/Firebase';
 
 function App() {
+  const [{user},dispatch] = useContext(DataContext);
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      if (authUser) {
+        dispatch({
+          type:Type.SET_USER,
+          user:authUser
+        });
+      } else {
+        dispatch({
+          type:Type.SET_USER,
+          user:null
+        });
+      }
+    })
+
+  },[])
+
+
   return (
     <div className="App">
       <Routers/>
