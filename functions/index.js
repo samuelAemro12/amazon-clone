545d7f8e09f54901c271dde90fc15b5ea66a942a
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-01-12 02:12:01 -0800

    backend code initialized

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,17 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+// a backend framework for Node.js
+const cors = require("cors");
+// a middleware that enables CORS with various options
+const dotenv = require("dotenv");
+// intsalled to use the .env file
+dotenv.config();
+// loads environment variables from a .env file into process.env
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// used stripe to make payments, and the secret key is in the .env file
+const app = express();
+app.use(cors({origin:true}));
+
+app.use(express.json());
 
