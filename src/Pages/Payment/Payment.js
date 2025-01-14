01b15a27951750ba1ec348d2e466b8e0944dbc98
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-01-14 11:05:10 -0800

    layout done for the Payment page

--- src/Pages/Payment/Payment.js
+++ src/Pages/Payment/Payment.js
@@ -4,7 +4,24 @@ import LayOut from '../../components/LayOut/LayOut';
 const Payment = () => {
   return (
     <LayOut>
-      
+      {/* header */}
+      <div>product</div>
+      {/* payment method  */}
+      <section>
+        {/* address */}
+        <div></div>
+        <hr/>
+
+        {/* product*/}
+        <div></div>
+        <hr/>
+
+        {/* card form */}
+        <div></div>
+        <hr/>
+      </section>
+
+   
     </LayOut>
   )
 }
