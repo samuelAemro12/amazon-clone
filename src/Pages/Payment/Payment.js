ca5786f7a4a00bfbd2c5ffc8a6655b0ff136262b
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-01-15 02:40:30 -0800

    debugged the problem with mapping the productCard component

--- src/Pages/Payment/Payment.js
+++ src/Pages/Payment/Payment.js
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Classes from './Payment.module.css'
 import LayOut from '../../components/LayOut/LayOut';
+import { DataContext } from '../../components/DataProvider/DataProvider';
+import ProductCard from '../../components/Product/ProductCard.js';
+
 const Payment = () => {
+  const [{ user, basket}] = useContext(DataContext);
+
+  const totalItem = basket?.reduce((amount,item)=>{
+    return item.amount + amount;
+  },0);
+
   return (
     <LayOut>
       {/* header */}
-      <div>product</div>
+      <div className={Classes.Payment__header}>
+      Cheeckout ({totalItem}) items 
+      </div>
       {/* payment method  */}
-      <section>
+      <section className={Classes.payment}>
         {/* address */}
-        <div></div>
+        <div className={Classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>{user.email}</div>
+           
+            <div>Address Street</div>
+            <div>Addis Ababa, Ethiopia</div>
+          </div>
+        </div>
         <hr/>
 
         {/* product*/}
-        <div></div>
+        <div className={Classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+        {
+          basket?.Map((item)=>{
+            return <ProductCard key={item.id} product={item} flex={true}/>
+          })
+        }
+          </div>
+        </div>
         <hr/>
 
         {/* card form */}
-        <div></div>
+        <div className={Classes.flex}>
+          <h3>Payment Methods</h3>
+          <div>
+            
+          </div>
+        </div>
         <hr/>
       </section>
 
    
     </LayOut>
-  )
+  );
 }
-
-export default Payment
+export default Payment;
