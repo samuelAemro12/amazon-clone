3a8de30320dedcf6835e7d078f45673431d73521
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-01-17 05:51:37 -0800

    protected the payment route to prevent unauthenticated users from paying because they'd have no id to be rcognized

--- src/Router.js
+++ src/Router.js
@@ -7,27 +7,32 @@ import Orders from './Pages/Orders/Orders';
 import Cart from './Pages/Cart/Cart';
 import Results from './Pages/Results/Results';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+
 const stripePromise = loadStripe('pk_test_51QJGOQANTtKnGrMkpnFnJWnKmMrorMem8v4EhyBbCioWpLDjZDoWNlh0VttpPfAlvWwrpR6qdgVcggTqmqN8kHn900IZaBIHc5');
 
 const Routers = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path= "/auth" element={<Auth/>}/>
-        <Route path= "/payment" element={
-          <Elements stripe={stripePromise}>
-            <Payment/>
-          </Elements>}/>
-        <Route path= "/orders" element={<Orders/>}/>
-        <Route path= "/cart" element={<Cart/>}/>
-        <Route path= "/category/:categoryName" element={<Results/>}/>
-        <Route path= "/products/:productId" element={<ProductDetail/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/payment" element={
+          <ProtectedRoute msg={"you must login to pay"} redirect={"/payment"}>
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          </ProtectedRoute>
+        } />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default Routers;
