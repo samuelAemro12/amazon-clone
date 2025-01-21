567086ae0f8ba5299e9142e75de462381149dc76
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-01-21 06:03:13 -0800

    changed the baseURL because i shifted the backend to render hosting

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,10 @@
 import axios from  "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-31206/us-central1/api"
+    // local instance of the API  firebase (cloud function) 
+   // baseURL: "http://127.0.0.1:5001/clone-31206/us-central1/api"
+   // reder deploymet instance of the API
+   baseURL: "https://amazon-backend-deploy-wd9j.onrender.com/"
 });
 
 export {axiosInstance}
