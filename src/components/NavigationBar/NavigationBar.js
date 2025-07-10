60d8902a41a901ac325f1c3f83a7635365c43b81
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-07-10 21:30:34 +0300

    bug fix: responsiveness

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Classes from './NavigationBar.module.css'; 
 import MenuIcon from '../../Assets/menu.png';
 
+const MOBILE_BREAKPOINT = 576;
+
 const NavigationBar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= MOBILE_BREAKPOINT);
+
+  // Update isMobile on window resize
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= MOBILE_BREAKPOINT);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   // Close drawer on ESC key
   const handleKeyDown = useCallback((e) => {
@@ -33,48 +43,54 @@ const NavigationBar = () => {
   ];
 
   return (
-    <>
-      <nav className={Classes.lower__container}>
-        <button
-          className={Classes.menu__button}
-          aria-label="Open navigation menu"
-          onClick={() => setDrawerOpen(true)}
-        >
-          <img src={MenuIcon} alt="Menu Icon" className={Classes.menu__icon} />
-          <span>All</span>
-        </button>
+    <nav className={Classes.lower__container}>
+      {/* Only show menu button and drawer on mobile */}
+      {isMobile && (
+        <>
+          <button
+            className={Classes.menu__button}
+            aria-label="Open navigation menu"
+            onClick={() => setDrawerOpen(true)}
+          >
+            <img src={MenuIcon} alt="Menu Icon" className={Classes.menu__icon} />
+            <span>All</span>
+          </button>
+          {/* Side Drawer and Overlay */}
+          <div
+            className={drawerOpen ? Classes.drawer__overlay + ' ' + Classes.drawer__overlay__active : Classes.drawer__overlay}
+            onClick={() => setDrawerOpen(false)}
+            tabIndex={drawerOpen ? 0 : -1}
+            aria-hidden={!drawerOpen}
+          />
+          <aside
+            className={drawerOpen ? Classes.side__drawer + ' ' + Classes.side__drawer__open : Classes.side__drawer}
+            aria-hidden={!drawerOpen}
+            tabIndex={-1}
+          >
+            <button
+              className={Classes.drawer__close__button}
+              aria-label="Close navigation menu"
+              onClick={() => setDrawerOpen(false)}
+            >
+              &times;
+            </button>
+            <ul className={Classes.drawer__nav__list}>
+              {navItems.map((item) => (
+                <li key={item} onClick={() => setDrawerOpen(false)}>{item}</li>
+              ))}
+            </ul>
+          </aside>
+        </>
+      )}
+      {/* Only show horizontal nav list on desktop */}
+      {!isMobile && (
         <ul className={Classes.nav__list}>
           {navItems.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ul>
-      </nav>
-      {/* Side Drawer and Overlay */}
-      <div
-        className={drawerOpen ? Classes.drawer__overlay + ' ' + Classes.drawer__overlay__active : Classes.drawer__overlay}
-        onClick={() => setDrawerOpen(false)}
-        tabIndex={drawerOpen ? 0 : -1}
-        aria-hidden={!drawerOpen}
-      />
-      <aside
-        className={drawerOpen ? Classes.side__drawer + ' ' + Classes.side__drawer__open : Classes.side__drawer}
-        aria-hidden={!drawerOpen}
-        tabIndex={-1}
-      >
-        <button
-          className={Classes.drawer__close__button}
-          aria-label="Close navigation menu"
-          onClick={() => setDrawerOpen(false)}
-        >
-          &times;
-        </button>
-        <ul className={Classes.drawer__nav__list}>
-          {navItems.map((item) => (
-            <li key={item} onClick={() => setDrawerOpen(false)}>{item}</li>
-          ))}
-        </ul>
-      </aside>
-    </>
+      )}
+    </nav>
   );
 }
 
